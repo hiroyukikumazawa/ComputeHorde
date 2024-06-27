fa698181ee44728fdd10499d47dff539ec914dc4
Andreea Popescu <andreea.popescu@reef.pl>
2024-06-27 06:54:48 +0100

    catch keyboard interrupts

--- validator/app/src/compute_horde_validator/validator/management/commands/debug_run_synthetic_job_to_miner.py
+++ validator/app/src/compute_horde_validator/validator/management/commands/debug_run_synthetic_job_to_miner.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+import sys
 from collections.abc import Iterable
 
 from django.conf import settings
@@ -52,7 +53,12 @@ class Command(BaseCommand):
             batch_id=None,
             keypair=key,
         )
-        asyncio.run(_execute_jobs(client, jobs))
+
+        try:
+            asyncio.run(_execute_jobs(client, jobs))
+        except KeyboardInterrupt:
+            print("Interrupted by user")
+            sys.exit(1)
 
 
 async def _execute_jobs(client: MinerClient, synthetic_jobs: Iterable[SyntheticJob]):
